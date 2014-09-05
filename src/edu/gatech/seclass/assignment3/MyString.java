7f2234f3cb6557660e7b3db28b837183a0563a98
Derek Thayer <dthayerstudent@gmail.com>
2014-09-04 21:10:24 -0400

    Updated MyString.java File

--- src/edu/gatech/seclass/assignment3/MyString.java
+++ src/edu/gatech/seclass/assignment3/MyString.java
@@ -18,20 +18,28 @@ public class MyString implements MyStringInterface{
     // Only letters a, e, i, o, and u (both lower and upper case) are considered vowels.
     // The returned string contains each occurrence of a vowel in the current string.
     public String getVowels(){
-    	return
+    	return current;
     }
 
     // Returns a string that consists of the substring between start and end indexes (both included) in the current string.
     // Index 1 corresponds to the first character in the current string.
-    public String getSubstring(int start, int end);
+    public String getSubstring(int start, int end){
+    	return current.substring(start -1,end -1);
+    }
 
     // Returns the index of the first occurrence of a character in the current string.
     // Index 1 corresponds to the first character in the current string.    
-    public int indexOf(char c);
-
+    public int indexOf(char c){
+    	return current.indexOf(c);
+    }
     // Removes all occurrences of the specified character from the current string.
-    public void removeChar(char c);
+    public void removeChar(char c){
+    	current = current.replace("c","");
+    }
 
     // Invert the current string.
-    public void invert();   
+    public void invert(){   
+    	StringBuilder build = new StringBuilder(current);
+    	build.reverse();
+    }
 }
