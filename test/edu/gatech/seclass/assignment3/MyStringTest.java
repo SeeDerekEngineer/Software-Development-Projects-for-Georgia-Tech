a7d893d9e2a6fef1c5c0e761acab176e639933eb
Derek Thayer <dthayerstudent@gmail.com>
2014-09-06 23:01:55 -0400

    Updated MyStringTest file

--- test/edu/gatech/seclass/assignment3/MyStringTest.java
+++ test/edu/gatech/seclass/assignment3/MyStringTest.java
@@ -1,34 +1,49 @@
 package edu.gatech.seclass.assignment3;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class MyStringTest {
 
+	//Verify special characters, capital letters, and double vowels retain functionality of getVowels()
 	@Test
 	public void testGetVowels() {
-		fail("Not yet implemented");
+		MyString tester = new MyString();
+		tester.setString("Aall eEl's iIn@#4 ooIut");
+		assertEquals("Output should read 'AaeEiIoIu'", "AaeEiIooIu", tester.getVowels());
+			
 	}
-
+	
+	//Verify index 1 corresponds to first character in string, end index is inclusive of substring
 	@Test
 	public void testGetSubstring() {
-		fail("Not yet implemented");
+		MyString tester = new MyString();
+		tester.setString("Swell Days");
+		assertEquals("Output should read 'well Day'","well Day",tester.getSubstring(2,8));
 	}
 
+	//Verify index 1 corresponds to first character in string, double letters, spaces, capital letters, 
+	//special characters retain functionality of indexOf()
 	@Test
 	public void testIndexOf() {
-		fail("Not yet implemented");
+		MyString tester = new MyString();
+		tester.setString("Aa #$% Mmm");
+		assertEquals("Output should read '9'", 9, tester.indexOf('m'));
 	}
 
 	@Test
 	public void testRemoveChar() {
-		fail("Not yet implemented");
+		MyString tester = new MyString();
+		tester.setString("Slaughter ssbunnies");
+		tester.removeChar('s');
+		assertEquals("Output should read 'laughter bunnie'","laughter bunnie",tester);
 	}
 
 	@Test
 	public void testInvert() {
-		fail("Not yet implemented");
+		MyString tester = new MyString();
+		tester.setString("Aall eEl's iIn@#4 ooIut");
 	}
 
 }
