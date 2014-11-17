d6c9438b8ffb03c6dd1d3614823320cdc383e54d
Derek Thayer <dthayerstudent@gmail.com>
2014-11-16 20:32:15 -0500

    Updated Files

--- GradesCalculator/src/edu/gatech/seclass/gradescalc/GradesDB.java
+++ GradesCalculator/src/edu/gatech/seclass/gradescalc/GradesDB.java
@@ -4,11 +4,12 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -24,10 +25,7 @@ public class GradesDB {
 	private XSSFSheet ic;
 	private XSSFSheet tg;
 	
-	public GradesDB(String gradesDb) {
-		
-	excel = new File (gradesDb);
-	}
+	public GradesDB(String gradesDb) {excel = new File (gradesDb);}
 	
 	public int getNumAssignments(){
 	
@@ -50,23 +48,190 @@ public class GradesDB {
 	return colNum;
 }
 
-	public static String cellToString (HSSFCell cell) {
+	public int getNumProjects(){
+		
+		try {
+			fis = new FileInputStream(excel);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wb = new XSSFWorkbook(fis);	
+			tg = wb.getSheet("TeamGrades");	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	      
+			
+	
+	int colNum = tg.getRow(0).getLastCellNum()-1;	      
+	return colNum;
+}
+	
+	public int getNumStudents(){
+		
+		try {
+			fis = new FileInputStream(excel);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wb = new XSSFWorkbook(fis);	
+			si = wb.getSheet("StudentsInfo");	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	      
+			
+	
+	int colNum = si.getLastRowNum();	      
+	return colNum;
+}
+	public HashSet<Student> getStudents(){
+		HashSet<Student> students = new HashSet<Student>();
+		Student student = new Student();
+		try {
+			fis = new FileInputStream(excel);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wb = new XSSFWorkbook(fis);	
+			si = wb.getSheet("StudentsInfo");	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	      
+		
+		int rowNum = si.getLastRowNum();
+		for (int i = 1; i < rowNum; i++){
+			XSSFRow row = si.getRow(i);
+			XSSFCell cell = row.getCell(0);
+			XSSFCell cell2 = row.getCell(1);
+			student.setName(cellToString(cell));
+			student.setGtid(cellToString(cell2));
+			students.add(student);
+			}
+		
+		
+		return students;
+	}
+	
+	
+	public Student getStudentByName(String name){
+		
+		Student studentByName = new Student();
+		
+		try {
+			fis = new FileInputStream(excel);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wb = new XSSFWorkbook(fis);	
+			si = wb.getSheet("StudentsInfo");	
+			a = wb.getSheet("Attendance");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	      
+		
+		int colNum = si.getLastRowNum();
+		for (int i = 0; i < colNum; i++){
+			XSSFRow row = si.getRow(i);
+			XSSFCell cell = row.getCell(0);
+			if(cellToString(cell).equals(name)){
+				XSSFCell cell2 = row.getCell(1);
+				
+				studentByName.setGtid(cellToString(cell2));
+			}
+		}
+		int colNum2 = a.getLastRowNum();
+		for (int j =1; j < colNum2; j++){
+			XSSFRow arow = a.getRow(j);
+			XSSFCell acell = arow.getCell(0);
+			if(cellToString(acell).equals(name)){
+				XSSFCell acell2 = arow.getCell(1);
+				
+				studentByName.setAttendance(cellToInt(acell2));
+				
+			}
+		}
+		return studentByName;
+	}
+	
+public Student getStudentByID(String id){
+		
+		Student studentById = new Student();
+		
+		try {
+			fis = new FileInputStream(excel);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			wb = new XSSFWorkbook(fis);	
+			si = wb.getSheet("StudentsInfo");	
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	      
+		
+		int colNum = si.getLastRowNum();
+		for (int i = 0; i < colNum; i++){
+			XSSFRow row = si.getRow(i);
+			XSSFCell cell = row.getCell(1);
+			if(cellToString(cell).equals(id)){
+				XSSFCell cell2 = row.getCell(0);
+				
+				studentById.setName(cellToString(cell2));
+			}
+		}
+		return studentById;
+	}
+	
+	public static String cellToString (XSSFCell cell) {
 		
 		int type = cell.getCellType();
-		object result ;
+		String result;
 
 		switch (type) {
 		
 			case 0 : //numerical value in excel
-				result = cell.getNumbericCellValue();
+				DataFormatter result1 = new DataFormatter();
+				result = result1.formatCellValue(cell);
 				break;
 			case 1: //String Value in excel
 				result = cell.getStringCellValue();
 				break;
 			default:
-				throw new RuntimeException(“Not an applicable cell content”);
+				throw new RuntimeException("Not an applicable cell content");
 		}
-		return result.toString();
+		return result;
 	} 
 
+	public static int cellToInt (XSSFCell cell) {
+		
+		int type = cell.getCellType();
+		int result;
+
+		switch (type) {
+		
+			case 0 : //numerical value in excel
+				double result1 = cell.getNumericCellValue();
+				result = (int) result1;
+				break;
+			case 1: //String Value in excel
+				throw new RuntimeException("Not a number");
+			default:
+				throw new RuntimeException("Not an applicable cell content");
+		}
+		return result;
+	} 
+	
 }
--- GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
+++ GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
@@ -2,4 +2,31 @@ package edu.gatech.seclass.gradescalc;
 
 public class Student {
 
+	private String name;
+	private String Gtid;
+	private String email;
+	private int C;
+	private int CPlusPlus;
+	private int java;
+	private char jobExpected;
+	private int attendance;
+	private int[] grades;
+	private int[] projectContribs;
+
+
+	public void setName(String newName){name = newName;} 
+	public String getName(){ return name;}
+	public void setGtid(String newGtid){Gtid = newGtid;}
+	public String getGtid(){ return Gtid;}
+	public void setEmail(String newEmail){email = newEmail;}
+	public String getEmail(){ return email;}
+	public int getC(){ return C;}
+	public int getCPlusPlus(){ return CPlusPlus;}
+	public int getJava(){ return java;}
+	public char getJobExpected(){ return jobExpected;}
+	public void setAttendance(int newAttendance){attendance = newAttendance;}
+	public int getAttendance(){ return attendance;}
+	public int[] getGrades(){ return grades;}
+	public int[] getProjectContribs(){ return projectContribs;}
+	
 }
