1f8faedaa4a1bb5072de717f21e700f812a7239b
Derek Thayer <dthayerstudent@gmail.com>
2014-12-07 20:42:47 -0500

    Updated Student Class

--- GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
+++ GradesCalculator/src/edu/gatech/seclass/gradescalc/Student.java
@@ -36,6 +36,46 @@ public class Student {
 	public int getAverageProjectsGrade() {return averageProjectsGrade;}
 	public void setOverallGrade(int newOGrade) {overallGrade = newOGrade;}
 	public int getOverallGrade() {return overallGrade;}
+
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Gtid == null) ? 0 : Gtid.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (Gtid == null) {
+			if (other.Gtid != null)
+				return false;
+		} else if (!Gtid.equals(other.Gtid))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
 	
 	
 }
